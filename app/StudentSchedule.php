c2862437dd765bc5554e44ac24881e8ffc690583
austindmariano <36832845+austindmariano@users.noreply.github.com>
2020-04-08 01:00:30 +0800

    Update StudentSchedule.php

--- app/StudentSchedule.php
+++ app/StudentSchedule.php
@@ -8,7 +8,6 @@ class StudentSchedule extends Model
 {
   protected $fillable = ['enrollment_id', 'subject_id', 'prelim_grade', 'midterm_grade',
   'prefinal_grade', 'final_grade', 'semestral', 'remarks', 'figure', 'status', 'active', 'last_updated_by'];
-<<<<<<< HEAD
 
   public function enrollment(){
     return $this->belongsTo('App\Enrollment')->select('*')->with('student');
@@ -17,6 +16,4 @@ class StudentSchedule extends Model
   public function curriculum_subject(){
     return $this->belongsTo('App\CurriculumSubject', 'subject_id')->select('*')->with('subject');
   }
-=======
->>>>>>> 6e6a446a37989a5b1402531df210f3f926cf3c98
 }
